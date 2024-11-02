039141f9dce2210343d6581f8da7cdac0a330e1a
dakota-bayer <89050126+dakota-bayer@users.noreply.github.com>
2024-11-02 17:53:49 -0500

    cleaned up structure

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,5 @@
 module.exports = {
     preset: 'ts-jest',
     testEnvironment: 'node',
-    testMatch: ['**/FizzBuzz/*.test.js', '**/PalindromeNumber/*.test.js'], // Adjust the path to match your directory structure
+    testMatch: ['**/?(*.)+(spec|test).ts'], // Match .ts test files
 };
